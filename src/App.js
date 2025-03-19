99da228a9174d05745de0443af091a085dc6d2eb
nsmairat <nilasmairat@gmail.com>
2025-03-19 13:11:56 -0700

    Worked on styling more

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">My Super Dooper Dictionary</header>
+        <header className="App-header">My Super Dooper Dictionary 📖</header>
       </div>
       <main>
         <Dictionary />
